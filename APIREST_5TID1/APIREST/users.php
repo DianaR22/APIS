0e68efacd5c717ea82c81444ce938b20dd51e741
DianaR22 <105998771+DianaR22@users.noreply.github.com>
2022-10-11 14:25:57 -0500

    Update users.php

--- APIREST_5TID1/APIREST/users.php
+++ APIREST_5TID1/APIREST/users.php
@@ -2,40 +2,25 @@
 
 require_once '../clases/users.class.php';
 require_once '../clases/response.class.php';
-require_once "../clases/conexion/conexion.php";
 
-
-$con = new conexion;
 $_user = new users;
 $_respuestas = new response;
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-        if(isset($_POST) && !empty($_POST)){ //con esto valido dos cosas isset es para verificar si la acción post está declarado y para saber si se encuentra vacio
-            $name =$_POST['nw_userName'];
-            $lastNames = $_POST['nw_apellidos'];
-            $rfc = $_POST['nw_rfc'];
-            $nickname = $_POST['nw_nickName'];
-            $userType = $_POST['userType'];
-            $pass = $_POST['nw_pass'];
-            $bActivo=1;
-
-            $query = "INSERT INTO `personas` (`personName`, `personLastName`, `personRFC`, `bActive`)
-            VALUES ('$name','$lastNames','$rfc','$bActivo')";
-             $lastId=($con->postDataId($query));
-
-             $query2="INSERT INTO `users` (`personId`, `user`, `pass`, `userType`, `bActive`) 
-             VALUES ( '$lastId','$nickname ',md5('$pass'),'$userType','$bActivo')"; 
-             $con->postData($query2);
-            header("Location:../views/users.php"); 
-        }
-        }
-    else if($_SERVER['REQUEST_METHOD'] == "GET"){
-       
-        echo "Próximamente disponible:D";
+    $datos = $_POST;
+    $dataArray = $_user->post($datos);
+    echo $dataArray;
+    if($dataArray > 0){
+        header("Location:/APIREST_5TID1/views/users.php"); 
     }
     else{
-        echo "Método no permitido";
+        header("Location:/APIREST_5TID1/views/users.php?err=1;"); 
     }
-    
 
+}elseif($_SERVER['REQUEST_METHOD'] == "GET"){
+    print_r($_GET);
+}else{
+    echo "Método no permitido";
+
+}
 ?>
